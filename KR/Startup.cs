c1163b84f38b0180842792bdb84db6e787e2e575
Khusainov Ruslan <khusainovheckfy@gmail.com>
2018-06-07 17:36:05 +0700

    complete stage 1

--- KR/Startup.cs
+++ KR/Startup.cs
@@ -15,6 +15,8 @@ namespace KR
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseDeveloperExceptionPage();
+
             app.UseDefaultFiles();
             app.UseStaticFiles();
         }
